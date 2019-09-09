440f0823f8e514de97e9bb0a84fae5d0635ff98c
EvanCaplan31 <evan@bwssolutions.com>
2019-09-08 22:40:15 -0400

    git ignore fix

--- internal/constants/constants.go
+++
@@ -1,5 +0,0 @@
-package constants
-
-const LogFileName string = "../../../stock-details-api.log"
-const AlphaVantageApiKey string = "PN0F0TOQPJWZ91OW"
-const BaseAlphaVantageUri string = "https://www.alphavantage.co/query?"
