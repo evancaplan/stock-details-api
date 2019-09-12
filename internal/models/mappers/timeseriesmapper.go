e664c72cadb03e4187bea5af85115946617244ff
EvanCaplan31 <evan@bwssolutions.com>
2019-09-12 17:55:26 -0400

    Remove sorting

--- internal/models/mappers/timeseriesmapper.go
+++ internal/models/mappers/timeseriesmapper.go
@@ -1,11 +1,6 @@
 package mappers
 
 import (
-	"sort"
-	"time"
-
-	"github.com/stock-details-api/internal/utils"
-
 	apiresponses "github.com/stock-details-api/internal/models/apiresponses"
 	dtos "github.com/stock-details-api/internal/models/dtos"
 )
@@ -23,26 +18,5 @@ func MapTimeSeriesDTOS(d apiresponses.Details) []dtos.ChartData {
 		MappedDetails = append(MappedDetails, data)
 
 	}
-	return sortDTOSNew(MappedDetails)
-}
-
-func sortDTOSNew(dtos []dtos.ChartData) []dtos.ChartData {
-	println("Sorting ChartData DTOS...")
-	log := utils.GetLogger()
-
-	layout := "2006-01-02"
-
-	sort.Slice(dtos, func(i, j int) bool {
-		timeOne, err := time.Parse(layout, dtos[i].Date)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		timeTwo, err := time.Parse(layout, dtos[j].Date)
-		if err != nil {
-			log.Fatal(err)
-		}
-		return timeOne.After(timeTwo)
-	})
-	return dtos
+	return MappedDetails
 }
